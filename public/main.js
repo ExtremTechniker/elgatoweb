e0a4b622284a7692d6fc48a345effe49ec5d5095
Robert Hesselmann <extremtechniker15@gmail.com>
2020-04-29 16:09:56 +0200

    added obs websocket js

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+require('./streamdeck')
+
 function openFullscreen(elem) {
     if (elem.requestFullscreen) {
       elem.requestFullscreen();
@@ -29,18 +31,3 @@ function test() {
     confirm();
 }
 
-function openTwitch() {
-    requestUrl("http://localhost:42800/send/OpenTwitch")
-}
-
-function openTwitchClym() {
-    requestUrl("http://localhost:42800/send/OpenTwitchClym")
-}
-
-function openExplorer() {
-    requestUrl("http://localhost:42800/send/openExplorer")
-}
-
-function play_pause() {
-    requestUrl("http://192.168.0.10:42800/send/PlayPauseMusic")
-}
---
+++ public/streamdeck.js
@@ -0,0 +1,17 @@
+//Edit Here YOUR SCRIPTS
+
+function openTwitch() {
+    requestUrl("http://localhost:42800/send/OpenTwitch")
+}
+
+function openTwitchClym() {
+    requestUrl("http://localhost:42800/send/OpenTwitchClym")
+}
+
+function openExplorer() {
+    requestUrl("http://localhost:42800/send/openExplorer")
+}
+
+function play_pause() {
+    requestUrl("http://192.168.0.10:42800/send/PlayPauseMusic")
+}
